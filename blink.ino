1428326c757c449a19db6960514c5f65ec5ffb02
Raulzaum <78221726+Raulzaum@users.noreply.github.com>
2021-01-29 12:48:42 -0300

    Monitor Serial
    Adicionado um texto utilizando o Monitor Serial

--- blink.ino
+++ blink.ino
@@ -1,6 +1,7 @@
 void setup() {
      pinMode(2,OUTPUT);
-
+     
+     Serial.begin(9600);
 }
 
 void loop() {
@@ -8,4 +9,7 @@ void loop() {
     delay(1000);
     digitalWrite(2,LOW);
     delay(1000);
+     
+     Serial.println("Exibir na tela");
+}
     
